e6317ad45542d45a8a8030120eea1fd36c7ffa56
yourCGG <56461454+yourCGG@users.noreply.github.com>
2024-11-29 15:46:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const dockerHub = "https://registry-1.docker.io";
 const routes = {
   // production
   "docker.boyka.com.cn": dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "quay.boyka.com.cn": "https://quay.io",
+  "gcr.boyka.com.cn": "https://gcr.io",
+  "k8s-gcr.boyka.com.cn": "https://k8s.gcr.io",
+  "k8s.boyka.com.cn": "https://registry.k8s.io",
+  "ghcr.boyka.com.cn": "https://ghcr.io",
+  "cloudsmith.boyka.com.cn": "https://docker.cloudsmith.io",
+  "ecr.boyka.com.cn": "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
